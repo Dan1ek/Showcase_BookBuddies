501abf6c2151a2fcb6d3b66ecdcdabaf0dcbfefb
Dan1ek <116569381+Dan1ek@users.noreply.github.com>
2024-03-15 14:24:16 +0100

    toevoegen boekenlijst

---
+++ Showcase_BookBuddies/Business/Entities/BookList.cs
@@ -0,0 +1,15 @@
+﻿using Showcase_BookBuddies.Business.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace Showcase_BookBuddies.Business.Entities
+{
+    public class BookList
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = ValidationMessages.Required)]
+        [StringLength(200, MinimumLength = 1, ErrorMessage = ValidationMessages.MinLength)]
+        public string Titel { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = ValidationMessages.Required)]
+        [StringLength(500, MinimumLength = 1, ErrorMessage = ValidationMessages.MinLength)]
+        public string Beschrijving { get; set; }
+    }
+}
